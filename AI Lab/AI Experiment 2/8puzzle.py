1f77e1e98dbf7c58417977a023f525dda1aa405c
Dyotak <dyotak10@gmail.com>
2023-08-05 17:32:26 +0530

    ai exp 2 done

--- AI Lab/AI Experiment 2/8puzzle.py
+++ AI Lab/AI Experiment 2/8puzzle.py
@@ -1,44 +1,130 @@
-import random
+import queue
+class Maze:
 
-def generate_maze(width, height):
-    maze = []
+    def __init__(self):    
+        self.start = "S"
+        self.goal = "G"
+        self.path = "."
+        self.block = "#"
+        self.START = "S"
+        self.GOAL = "G"
+        self.PATH = "."
+        self.BLOCK = "#"
 
-    for _ in range(height):
-        row = []
-        for _ in range(width):
-            row.append("#")
-        maze.append(row)
+    def create_maze(self) -> list:
+        maze = []
+        
+        maze.append([self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.START, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK])
+        maze.append([self.BLOCK, " ", " ", " ", self.BLOCK, " ", " ", " ", " ", " ", self.BLOCK])
+        maze.append([self.BLOCK, self.BLOCK, self.BLOCK, " ", self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, " ", self.BLOCK])
+        maze.append([self.BLOCK, " ", " ", " ", self.BLOCK, " ", " ", " ", " ", " ", self.BLOCK])
+        maze.append([self.BLOCK, " ", self.BLOCK, self.BLOCK, self.BLOCK, " ", self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK])
+        maze.append([self.BLOCK, " ", " ", " ", " ", " ", " ", " ", " ", " ", self.BLOCK])
+        maze.append([self.BLOCK, " ", self.BLOCK, self.BLOCK, self.BLOCK, " ", self.BLOCK, self.BLOCK, self.BLOCK, " ", self.BLOCK])
+        maze.append([self.BLOCK, " ", self.BLOCK, " ", " ", " ", self.BLOCK, " ", " ", " ", self.BLOCK])
+        maze.append([self.BLOCK, " ", self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, " ", self.BLOCK, self.BLOCK, self.BLOCK])
+        maze.append([self.BLOCK, " ", " ", " ", " ", " ", self.BLOCK, " ", " ", " ", self.BLOCK])
+        maze.append([self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.GOAL, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK, self.BLOCK])
 
-    start_x, start_y = random.randint(1, width - 2), 0
-    goal_x, goal_y = random.randint(1, width - 2), height - 1
+        return maze
 
-    maze[start_y][start_x] = "S"
-    maze[goal_y][goal_x] = "G"
+    def is_allowed(self, maze, moves=""):
+        start_index = maze[0].index(self.start)
+        x = start_index
+        y = 0
 
-    stack = [(start_x, start_y)]
+        for i in moves:
+            if i == "L": x -= 1
+            if i == "R": x += 1
+            if i == "U": y -= 1
+            if i == "D": y += 1
 
-    while stack:
-        x, y = stack[-1]
-        maze[y][x] = " "
+        if x <= 0 or y <= 0:
+            return False
+        
+        if x >= len(maze) or y >= len(maze[0]):
+            return False
 
-        neighbors = [(x + dx, y + dy) for dx, dy in [(0, -1), (0, 1), (-1, 0), (1, 0)]]
-        unvisited_neighbors = [(nx, ny) for nx, ny in neighbors if 0 <= nx < width and 0 <= ny < height and maze[ny][nx] == "#"]
+        if maze[y][x] == self.block:
+            return False
 
-        if unvisited_neighbors:
-            nx, ny = random.choice(unvisited_neighbors)
-            stack.append((nx, ny))
-            maze[(ny + y) // 2][(nx + x) // 2] = " "
-        else:
-            stack.pop()
+        return True
+    
+    def is_solved(self, maze, moves=""):
+        start_index = maze[0].index(self.start)
+        x = start_index
+        y = 0
 
-    return maze
+        print(moves)
 
-def print_maze(maze):
-    for row in maze:
-        print("".join(row))
+        for i in moves:
+            if i == "L": x -= 1
+            if i == "R": x += 1
+            if i == "U": y -= 1
+            if i == "D": y += 1
 
-if __name__ == "__main__":
-    width, height = 11, 11
-    maze = generate_maze(width, height)
-    print_maze(maze)
+        if x <= 0 or y <= 0:
+            return False
+        
+        if x > len(maze) or y > len(maze[0]):
+            return False
 
+        if maze[y][x] == self.block:
+            return False
+        
+        if maze[y][x] == self.goal:
+            print(moves)
+            return True
+                
+
+    def print_maze(self, maze, moves=''):
+        print()
+        my_maze = maze
+        start_index = maze[0].index(self.start)
+        x = start_index
+        y = 0
+
+        for i in moves:
+            if i == "L": x -= 1
+            if i == "R": x += 1
+            if i == "U": y -= 1
+            if i == "D": y += 1
+            my_maze[y][x] = self.path
+
+        for row in maze:
+            print(" ".join(row))
+        print()
+
+        
+
+
+m = Maze()
+maze = m.create_maze()
+
+my_queue = queue.Queue()
+my_queue.put("")
+path = ""
+
+possible_moves = ['L', 'R', 'U', 'D']
+
+m.print_maze(maze)
+
+while not m.is_solved(maze=maze, moves=path):
+    path = my_queue.get()
+    
+    for i in possible_moves:
+        if (i == 'L' and path.endswith('R')):
+            continue
+        if (i == 'R' and path.endswith('L')):
+            continue
+        if (i == 'D' and path.endswith('U')):
+            continue
+        if (i == 'U' and path.endswith('D')):
+            continue
+
+        temp = path + i
+        
+        if m.is_allowed(maze, temp):
+            my_queue.put(temp)
+
+m.print_maze(maze, path)
--- AI Lab/AI Experiment 2/maze_bfs.py
+++ AI Lab/AI Experiment 2/maze_bfs.py
@@ -1,36 +1,30 @@
 import queue
-from typing import NamedTuple
-
-class Coordinate(NamedTuple):
-    x: int
-    y: int
-
 class Maze:
 
     def __init__(self):    
-        self.start = ""
-        self.goal = ""
-        self.path = ""
-        self.block = "󰚍"
+        self.start = "S"
+        self.goal = "G"
+        self.path = "."
+        self.block = "#"
 
     def create_maze(self) -> list:
         maze = []
         
-        maze.append(["󰚍", "󰚍", "󰚍", "󰚍", "󰚍", self.start, "󰚍", "󰚍", "󰚍", "󰚍", "󰚍"])
-        maze.append(["󰚍", " ", " ", " ", "󰚍", " ", " ", " ", " ", " ", "󰚍"])
-        maze.append(["󰚍", "󰚍", "󰚍", " ", "󰚍", "󰚍", "󰚍", "󰚍", "󰚍", " ", "󰚍"])
-        maze.append(["󰚍", " ", " ", " ", "󰚍", " ", " ", " ", " ", " ", "󰚍"])
-        maze.append(["󰚍", " ", "󰚍", "󰚍", "󰚍", " ", "󰚍", "󰚍", "󰚍", "󰚍", "󰚍"])
-        maze.append(["󰚍", " ", " ", " ", " ", " ", " ", " ", " ", " ", "󰚍"])
-        maze.append(["󰚍", " ", "󰚍", "󰚍", "󰚍", " ", "󰚍", "󰚍", "󰚍", " ", "󰚍"])
-        maze.append(["󰚍", " ", "󰚍", " ", " ", " ", "󰚍", " ", " ", " ", "󰚍"])
-        maze.append(["󰚍", " ", "󰚍", "󰚍", "󰚍", "󰚍", "󰚍", " ", "󰚍", "󰚍", "󰚍"])
-        maze.append(["󰚍", " ", " ", " ", " ", " ", "󰚍", " ", " ", " ", "󰚍"])
-        maze.append(["󰚍", "󰚍", "󰚍", "󰚍", "󰚍", self.goal, "󰚍", "󰚍", "󰚍", "󰚍", "󰚍"])
+        maze.append(["#", "S", "#", "#", "#", "#", "#", "#", "#", "#", "#"])
+        maze.append(["#", " ", " ", " ", "#", " ", " ", " ", " ", " ", "#"])
+        maze.append(["#", "#", "#", " ", "#", "#", "#", "#", "#", " ", "#"])
+        maze.append(["#", " ", " ", " ", "#", " ", " ", " ", " ", " ", "#"])
+        maze.append(["#", " ", "#", "#", "#", " ", "#", "#", "#", "#", "#"])
+        maze.append(["#", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"])
+        maze.append(["#", " ", "#", "#", "#", " ", "#", "#", "#", " ", "#"])
+        maze.append(["#", " ", "#", " ", " ", " ", "#", " ", " ", " ", "#"])
+        maze.append(["#", " ", "#", "#", "#", "#", "#", " ", "#", "#", "#"])
+        maze.append(["#", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#"])
+        maze.append(["#", "#", "#", "#", "#", "G", "#", "#", "#", "#", "#"])
 
         return maze
 
-    def is_allowed(self, maze, moves="") -> bool:
+    def is_allowed(self, maze, moves=""):
         start_index = maze[0].index(self.start)
         x = start_index
         y = 0
@@ -40,7 +34,31 @@ class Maze:
             if i == "R": x += 1
             if i == "U": y -= 1
             if i == "D": y += 1
-                
+
+        if x <= 0 or y <= 0:
+            return False
+        
+        if x >= len(maze) or y >= len(maze[0]):
+            return False
+
+        if maze[y][x] == self.block:
+            return False
+
+        return True
+    
+    def is_solved(self, maze, moves=""):
+        start_index = maze[0].index(self.start)
+        x = start_index
+        y = 0
+
+        print(moves)
+        # self.print_maze(maze, moves)
+
+        for i in moves:
+            if i == "L": x -= 1
+            if i == "R": x += 1
+            if i == "U": y -= 1
+            if i == "D": y += 1
 
         if x <= 0 or y <= 0:
             return False
@@ -51,17 +69,59 @@ class Maze:
         if maze[y][x] == self.block:
             return False
         
-        return True
-
+        if maze[y][x] == self.goal:
+            print(moves)
+            return True
                 
 
-    def print_maze(self, maze):
+    def print_maze(self, maze, moves=''):
+        print()
+        my_maze = maze
+        start_index = maze[0].index(self.start)
+        x = start_index
+        y = 0
+
+        for i in moves:
+            if i == "L": x -= 1
+            if i == "R": x += 1
+            if i == "U": y -= 1
+            if i == "D": y += 1
+            my_maze[y][x] = self.path
+
         for row in maze:
             print(" ".join(row))
+        print()
 
         
 
 
 m = Maze()
 maze = m.create_maze()
+
+my_queue = queue.Queue()
+my_queue.put("")
+path = ""
+
+possible_moves = ['L', 'R', 'U', 'D']
+
 m.print_maze(maze)
+
+while not m.is_solved(maze=maze, moves=path):
+    path = my_queue.get()
+    
+    for i in possible_moves:
+        if (i == 'L' and path.endswith('R')):
+            continue
+        if (i == 'R' and path.endswith('L')):
+            continue
+        if (i == 'D' and path.endswith('U')):
+            continue
+        if (i == 'U' and path.endswith('D')):
+            continue
+
+        temp = path + i
+        
+        if m.is_allowed(maze, temp):
+            my_queue.put(temp)
+
+m.print_maze(maze, path)
